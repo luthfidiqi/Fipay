9ec3f748d8fd4c3320fb61d9ad5a50a484bde8a5
luthfidiqi <luthfidiqi@gmail.com>
2022-07-03 12:06:03 +0700

    topup & profile

--- pages/main/topup/index.jsx
+++ pages/main/topup/index.jsx
@@ -6,7 +6,20 @@ export default function Topup() {
     <>
       <div>
         <Layout>
-          <h1>Top Up</h1>
+          <div className="topup">
+            <div className="menu-title-main d-flex justify-content-between align-items-center">
+              <h3>Topup</h3>
+            </div>
+            <p className="mt-2 mb-4">
+              Enter the amount of money, and click submit
+            </p>
+            <form className="topup-form">
+              <input type="email" className="form-control" />
+              <button type="submit" className="purple-btn mt-4">
+                submit
+              </button>
+            </form>
+          </div>
         </Layout>
       </div>
     </>
